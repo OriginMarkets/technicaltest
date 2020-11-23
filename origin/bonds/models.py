3f7cda87c7fcb93fec84fb15d3a6fd1d57b57518
venkat <merugac@gmail.com>
2020-11-23 17:39:05 +0000

    my work solution

--- origin/bonds/models.py
+++ origin/bonds/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Bonds(models.Model):
+    isin = models.CharField(max_length=12, primary_key=True)
+    size = models.PositiveIntegerField(null=False, blank=False)
+    currency = models.CharField(max_length=6, null=False, blank=False)
+    maturity = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    lei = models.CharField(max_length=20, null=False, blank=False)
+    legal_name = models.CharField(max_length=30, null=True, blank=True)
--- origin/bonds/tests.py
+++ origin/bonds/tests.py
@@ -1,7 +1,30 @@
 from rest_framework.test import APISimpleTestCase
+from rest_framework.test import APISimpleTestCase, APIClient
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+from django.test import TestCase
 
 
 class HelloWorld(APISimpleTestCase):
     def test_root(self):
         resp = self.client.get("/")
         assert resp.status_code == 200
+
+class BondsTest(APISimpleTestCase, TestCase):
+    def setUp(self):
+        User.objects.create(username='svet', email='svet@tsvet.com', password='svet234')
+        user = User.objects.get(username='svet')
+        Token.objects.create(user=user)
+
+    def test_unathorised_access(self):
+        response = self.client.get("/bonds/")
+        assert response.status_code == 401
+
+    def test_bonds_index_view(self):
+        token = Token.objects.get(user__username='svet')
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response = self.client.get("/bonds/")
+        assert response.status_code == 200
