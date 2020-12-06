057ce99709ecc02f426c02a252485fdcff686343
Marco De Felice <md-f@users.noreply.github.com>
2020-12-06 17:55:31 +0000

    Add get_legalname unit test

--- origin/bonds/utils/tests.py
+++ origin/bonds/utils/tests.py
@@ -1,5 +1,5 @@
-from operator import itemgetter
 from .currency import get_currency_choices
+from .lei import get_legalname
 
 
 def test_currency_choices():
@@ -8,3 +8,10 @@ def test_currency_choices():
     assert len(choices) == 268
     assert choices[0] == ('XUA', 'ADB Unit of Account')
     assert choices[-1] == ('PLN', 'Zloty')
+
+
+def test_lei_get_legalname():
+    result = get_legalname('21380016UZS36PC85Y22')
+    assert result == 'ORIGIN GROUP LIMITED'
+    result = get_legalname('894500TFYBOUIM1WUN34')
+    assert result == 'DEUTSCH-BELGISCH-LUXEMBURGISCHE HANDELSKAMER - BELGISCH-LUXEMBURGS-DUITSE KAMER VAN KOOPHANDEL - CHAMBRE DE COMMERCE BELGO-LUXEMBOURGEOISE-ALLEMANDE'
