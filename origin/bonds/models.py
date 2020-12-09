cc2c52796e946b68b5b2b8ae8dbc09a9ce918e30
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-12-09 15:45:58 +0000

    Create bond model

---
+++ origin/bonds/models.py
@@ -0,0 +1,48 @@
+from django.db.models import CharField, DateTimeField, Model, DecimalField, DateField
+from babel import numbers
+
+class Bond(Model):
+    created = DateTimeField(auto_now_add=True)
+    
+    # Bond ISIN Code 
+    # Example: FR0000131104
+    # 12 character code
+    # https://www.isin.net/bond-isin-code/
+    isin = CharField(max_length=12)
+    
+    # Bond size
+    # Example: 100000000
+    # For a whole number we could use size = IntegerField() however
+    # it's sensible to use two decimal places here
+    size = DecimalField(max_digits=15, decimal_places=2)
+
+    # Currency code
+    # Example: EUR
+    # 3 character string
+    # For now this can be implemented as a character field
+    # To allow more flexability we can use py-moneyed in the future
+    currency = CharField(max_length=3)
+    
+    # Example: 2025-02-28
+    # yyyy-mm-dd date string
+    maturity = DateField()
+    
+    # LEI
+    # Example: R0MUWSFPU8MPRO8K5P83
+    # 20 character identifier
+    # https://www.bourse.lu/lei
+    lei = CharField(max_length=20)
+    
+    # Legal Name
+    # Exmple: BNPPARIBAS
+    # Example2: Société Générale Effekten GmbH
+    legal_name = CharField(max_length=100)
+
+    def __str__(self):
+        return f"Bond with LEI: {self.lei}"
+
+    def formatted_size(self):
+        return numbers.format_currency(self.size, self.currency)
+
+    class Meta:
+        ordering = ["created"]
